1dfe53914d3676361b3500e5b6db93941b504909
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-04-19 00:43:29 -0300

    Message:

---
+++ app/assets/javascripts/test/unit/controllersSpec.js
@@ -0,0 +1,10 @@
+describe('DashboardCtrl', function(){
+  it('should return false to the isGameDone', function() {
+    var scope = {};
+    var UserGame = {};
+    var Motivator = {};
+    var user_game = {};
+    ctrl = new DashboardCtrl(scope, UserGame, Motivator);
+    expect(scope.isGameDone(user_game)).toBe(false);
+  });
+});
