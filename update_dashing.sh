1e3e5cf47217404257604bd19170c6ce6be9c00d
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-04-20 19:54:14 -0300

    Message:

--- update_dashing.sh
+++ update_dashing.sh
@@ -1,1 +1,4 @@
-curl -d '{ "auth_token": "dashinglabtoken", "current": "$2" }' http://dashinglab.herokuapp.com:80/widgets/$1
+curl -d '{ "auth_token": "dashinglabtoken", "current": "100" }' http://dashinglab.herokuapp.com:80/widgets/mm_views_coverage;
+curl -d '{ "auth_token": "dashinglabtoken", "current": "99.9" }' http://dashinglab.herokuapp.com:80/widgets/mm_controllers_coverage;
+curl -d '{ "auth_token": "dashinglabtoken", "current": "99.5" }' http://dashinglab.herokuapp.com:80/widgets/mm_total_coverage;
+
